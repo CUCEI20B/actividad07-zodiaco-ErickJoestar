6d636fbcdca9df1a0a96a943e34ef4f63b197df5
ErickJoestar <erickborquez@gmail.com>
2020-09-24 07:26:16 -0600

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-month = int(input())
 day = int(input())
+month = int(input())
 if month == 1 and day >= 20 or month == 2 and day <= 18:
     print("acuario")
 elif month == 2 and day >= 19 or month == 3 and day <= 21:
